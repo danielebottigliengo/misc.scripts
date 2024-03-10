b859c02293ec2a21fe18795f5b9a7730940643c4
danielebottigliengo <daniele.bottigliengo@libero.it>
2024-03-10 17:00:50 +0100

    Simulate baseline covariates

--- data/sim_observational_cad_tte_mace.R
+++ data/sim_observational_cad_tte_mace.R
@@ -12,8 +12,18 @@
 # be used when practicing with causal inference methods for
 # time-to-event endpoints in observational/non-randomized clinical trials
 
+# We consider an hypothetical trial where a new treatment is
+# compared with the standard one. The new treatment consists of a
+# surgical procedure that allows a less invasive intervention on
+# the patient than the standard. Clinicians are more likely to treat
+# patients with poor prognosis with the standard procedure, with which
+# they have established and successful experience. The aim of the
+# trial is to establish non-inferiority of the new treatment in terms
+# of 5-years MACE risk and average time-free-from MACE.
+
 # Load packages --------------------------------------------------------
 library(tidyverse)
+library(MASS)
 
 # 1) Simulate 12 clinical baseline covariates --------------------------
 # Simulation from a latent multivariate normal to mimic a CAD population
@@ -41,15 +51,63 @@ corr_matrix <- matrix(
     0.2, 1, 0.05, -0.2, -0.1, -0.3, 0, -0.1, -0.1, -0.2, 0, -0.1,
     0.25, 0.05, 1, 0.7, 0.8, 0.4, 0, 0.3, 0.3, 0.3, -0.2, 0.5,
     0.5, -0.2, 0.7, 1, 0.7, 0.45, 0, 0.3, 0.3, 0.3, -0.45, 0.8,
-    0.6, -0.1, 0.8, 0.7, 1, 0.6, 0.2, 0.55, 0.55, 0.55, -0.5, 0.65
+    0.6, -0.1, 0.8, 0.7, 1, 0.6, 0.2, 0.55, 0.55, 0.55, -0.5, 0.65,
+    0.4, -0.1, 0.4, 0.45, 0.6, 1, 0.2, 0.6, 0.4, 0.65, -0.25, 0.4,
+    0, 0, 0, 0, 0.2, 0.2, 1, 0.25, 0.25, 0.25, -0.2, 0.3,
+    0.3, -0.1, 0.3, 0.3, 0.55, 0.6, 0.25, 1, 0.6, 0.5, -0.5, 0.4,
+    0.4, -0.1, 0.3, 0.3, 0.55, 0.4, 0.25, 0.6, 1, 0.5, -0.5, 0.4,
+    0.5, -0.2, 0.3, 0.3, 0.55, 0.65, 0.25, 0.5, 0.5, 1, -0.3, 0.3,
+    -0.6, 0, -0.2, -0.45, -0.5, -0.25, -0.2, -0.5, -0.5, -0.3, 1, -0.35,
+    0.4, -0.1, 0.5, 0.8, 0.65, 0.4, 0.3, 0.4, 0.4, 0.3, -0.35, 1
   ),
   nrow = 12,
   ncol = 12
 )
 
+covs <- c(
+  "age", "female", "bmi", "diabetes", "hypertension", "smoking",
+  "family_cad", "previous_mi", "previous_pci", "history_tia_cva",
+  "lvef", "dyslipidemia"
+)
+colnames(corr_matrix) <- covs
+rownames(corr_matrix) <- covs
 
+# Simulate baseline covariates from a standard multivariate normal
+sds <- rep(1, ncol(corr_matrix))
+mus <- rep(0, ncol(corr_matrix))
 
+# # From correlation to covariance following this:
+# # https://stats.stackexchange.com/questions/62850/obtaining-covariance-matrix-from-correlation-matrix
+# cov_matrix <- sweep(sweep(corr_matrix, 1, sds, "*"), 2, sds, "*")
 
+# In our case, since this is a multivariate standard normal, the
+# covariance matrix equals the correlation matrix
+cov_matrix <- corr_matrix
+
+n <- 1500L
+mv_bs <- mvrnorm(n = n, mu = mus, Sigma = cov_matrix)
+
+# Get the uniform distributions
+u_bs <- pnorm(mv_bs)
+
+# Get the marginal distributions for each covariante and store
+cov_bs <- u_bs
+cov_bs[, 1] <- qnorm(p = u_bs[, 1], mean = 65, sd = 8.2)
+cov_bs[, 2] <- qbinom(p = u_bs[, 2], size = 1, prob = 0.18)
+cov_bs[, 3] <- qnorm(p = u_bs[, 3], mean = 29, sd = 1.8)
+cov_bs[, 4] <- qbinom(p = u_bs[, 4], size = 1, prob = 0.28)
+cov_bs[, 5] <- qbinom(p = u_bs[, 5], size = 1, prob = 0.73)
+cov_bs[, 6] <- qbinom(p = u_bs[, 6], size = 1, prob = 0.60)
+cov_bs[, 7] <- qbinom(p = u_bs[, 7], size = 1, prob = 0.30)
+cov_bs[, 8] <- qbinom(p = u_bs[, 8], size = 1, prob = 0.33)
+cov_bs[, 9] <- qbinom(p = u_bs[, 9], size = 1, prob = 0.14)
+cov_bs[, 10] <- qbinom(p = u_bs[, 10], size = 1, prob = 0.07)
+cov_bs[, 11] <- qnorm(p = u_bs[, 11], mean = 55, sd = 10)
+cov_bs[, 12] <- qbinom(p = u_bs[, 10], size = 1, prob = 0.70)
+colnames(cov_bs) <- covs
+
+# 2) Simulate treatment assignment -------------------------------------
+# More severe patients are more
 
 
 
